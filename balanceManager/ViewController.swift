59f3602a389645b9c491ac63892f83944b32a29c
nao <unconfigured@null.spigotmc.org>
2022-08-10 15:06:29 +0900

    Initial Commit

---
+++ balanceManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  balanceManager
+//
+//  Created by なお on 2022/08/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
